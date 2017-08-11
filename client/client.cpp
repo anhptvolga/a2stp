8ac35c90b2e2713d44c74bc701c16012a1ff9640
LinhTTN <linhttn@localhost.localdomain>
2017-08-11 09:56:08 +0700

    singal_unit struct and init the client

---
+++ client/client.cpp
@@ -0,0 +1,22 @@
+#include "../su.h"
+#include "../bitlib.h"
+#include <cstdio>
+#include <ctime>
+
+
+// Randomly generate signal unit.
+struct signal_unit generate_data() {
+	srand(time(NULL));	
+
+	
+
+
+}
+
+
+// main loop. client will send data to server every 3s.
+int main(int argc, char *argv[]) {
+	printf("Hello, world!\n");
+
+	return 0;
+}
