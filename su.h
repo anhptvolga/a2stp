6898781ff732af6b26527587b24e391bb8b712ff
anhcx0209 <anhcx0209@gmail.com>
2017-08-11 10:45:58 +0700

    reformat file su.h

--- su.h
+++ su.h
@@ -1,11 +1,16 @@
 #ifndef __su_h__
 #define __su_h__
 
-struct signal_unit {
+struct party_address {
 	char indicator;
 	char pointCode[2];
 	char subNumber;
 	char gt[11];
+};
+
+struct signal_unit {
+	struct party_address CgPA;
+	struct party_address CdPA;
 
 	char data[20];
 };
