74c3258a9e2ae0ffa980d3178af238e3a1f618f6
godric <anhptvolga@yandex.ru>
2017-08-10 22:52:54 -0400

    add constants

---
+++ const.h
@@ -0,0 +1,19 @@
+#ifndef __const_h__
+#define __const_h__
+
+/* server address */
+#define SV_IP "192.168.2.54"
+#define SV_PORT 4321
+
+
+/* config filenames */
+#define CFILE_SCREENING "screening.conf"
+#define CFILE_GTT       "gtt.conf"
+
+/* output filenames */
+#define LFILE_BLOCK     "block.log"
+#define LFILE_DISCARD   "discard.log"
+#define LFILE_LOCAL     "local.log"
+#define LFILE_FORWARD   "forward.log"
+
+#endif
