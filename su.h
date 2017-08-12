dab8c32fdda5567e78510b585baa72669d739ef6
anhcx0209 <anhcx0209@gmail.com>
2017-08-12 11:15:10 +0700

    seperate functions from su.h

--- su.h
+++ su.h
@@ -3,10 +3,7 @@
 
 #define SU_SIZE 50
 #define PA_SIZE 15
-#define DATA_SIZE 20
-#include <cstdlib>
-#include <cstring>
-
+#define MESSAGE_SIZE 20
 
 // party address for calling and called
 // total size of pa = 15 bytes
@@ -25,26 +22,5 @@ struct signal_unit {
 
 	char data[20];
 };
-
-// connect all party_address
-const char *pa_to_buffer(struct party_address p) {
-	char *ret = (char *)malloc(PA_SIZE * sizeof(char));
-	memcpy(ret, &p.indicator, 1);
-	memcpy(ret + 2, p.pointCode, 2);
-	memcpy(ret + 3, &p.subNumber, 1);
-	memcpy(ret + 5, p.gt, 11);	
-	return ret;
-}
-
-// function transform signal_unit to buffer -> transfer throught socket
-const char *su_to_buffer(struct signal_unit s) {
-	char *pk = (char *)malloc(SU_SIZE * sizeof(char));
-	const char *cgpa_buf = pa_to_buffer(s.CgPA);
-	const char *cdpa_buf = pa_to_buffer(s.CdPA);
-	memcpy(pk, cgpa_buf, PA_SIZE);
-	memcpy(pk + 15, cdpa_buf, PA_SIZE);	
-	memcpy(pk + 30, s.data, DATA_SIZE);
-	return pk;
-}
 
 #endif
