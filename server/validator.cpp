b527a5e3f6b7bee811bb4babd4a5f8ea12eea6dc
godric <anhptvolga@yandex.ru>
2017-08-12 03:18:18 -0400

    sample config

--- server/validator.cpp
+++ server/validator.cpp
@@ -4,9 +4,34 @@
 
 #include "validator.h"
 
-int validate(signal_unit su) {
+using namespace std;
+
+int validate(signal_unit su, time_t time) {
+    //cout << "indicator: "; bin_print_char(su.CgPA.indicator); cout << endl;
+    //cout << "PC: "; bin_print_buff(su.CgPA.pointCode, 2); cout << endl;
+    //cout << "ssn: "; bin_print_char(su.CgPA.subNumber); cout << endl;
+    //cout << "gt: "; bin_print_buff(su.CgPA.gt, 11); cout << endl;
+
+    party_address calling = su.CgPA;
+    party_address called = su.CdPA;
+    if (!testbit(calling.indicator, BIT_G_POS)) {
+        // routing on GT, check gt of CgPA and CdPA
+        if (!memcmp(calling.gt, called.gt, GT_SIZE)) {
+            write_vl_log(su, time, ERR_SAME_GT);
+        }
+    } else {
+        // routing on SSN, check DPC and SSN
+        if (!memcmp(calling.pointCode, called.pointCode, PC_SIZE)) {
+            write_vl_log(su, time, ERR_SAME_PC);
+        }
+    }
     return 0;
 }
 
-void writeVlLog(signal_unit su, int errcode) {
+void write_vl_log(signal_unit su, time_t time, int errcode) {
+    ofstream ofs(LFILE_DISCARD, std::ofstream::app);
+    cout << ctime(&time) << " : "
+         << su.CgPA.gt << " : "
+         << errcode << endl;
+    ofs.close();
 }
--- server/validator.h
+++ server/validator.h
@@ -1,21 +1,27 @@
 #ifndef __validator_h__
 #define __validator_h__
 
+#include <iostream>
+#include <cstring>
+#include <fstream>
 #include "../su.h"
+#include "../const.h"
+#include "../bitlib.h"
 
 /* list of error codes */
-#define ERR_SAME_SD 1
-#define ERR_STP_SSN 2
+#define ERR_SAME_PC 1
+#define ERR_SAME_GT 2
+#define ERR_STP_SSN 3
 
 /**
  * validate signal unit
  * return 0 if ok or error code
  */
-int validate(signal_unit su);
+int validate(signal_unit su, time_t time);
 
 /**
  * write validating log with error code 
  */
-void writeVlLog(signal_unit su, int errcode);
+void write_vl_log(signal_unit su, time_t time, int errcode);
 
 #endif
