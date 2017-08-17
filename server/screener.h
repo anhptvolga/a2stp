18723822e5db461a4da12a649eb119aa7d70d914
godric <anhptvolga@yandex.ru>
2017-08-16 21:03:57 -0400

    delete unusing code and add comments in screener

--- server/screener.h
+++ server/screener.h
@@ -14,8 +14,20 @@
 typedef std::unordered_map<std::string, bool> mapstrbool;
 typedef std::unordered_map<std::string, bool>::iterator iterstrbool;
 
+/**
+ * hash function: return hash value of 2 subsystem number
+ */
 string h(int spc, int dpc);
+
+/**
+ * read user's blocking list config
+ */
 void read_block_list(const char *filename, mapstrbool &blist);
+
+/**
+ * check is signal blocked by user's config
+ * return false if signal's blocked and true if signal's non-blocked
+ */
 bool check_block(struct signal_unit su, time_t t, mapstrbool blist);
 
 #endif
